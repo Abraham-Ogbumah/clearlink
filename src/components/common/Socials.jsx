8a9b87c3d7483525dee5005a45b88d386e7d1ed5
Abraham-Ogbumah <ab.ogbumah@gmail.com>
2024-01-24 03:07:38 -0500

    added social links

---
+++ src/components/common/Socials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Socials = () => {
+  return (
+    <div className='social-links'>
+        <div>
+            <p className='copyright'>© 2023 ClearLink. All rights reserved.</p>
+        </div>
+        <div className='social-buttons'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Socials
